c993bd873e0f986951cda7ccfa41189b7fc93b33
kamal almorsy <kamalaboueidd@gmail.com>
2026-09-08 20:33:52 +0300

    Tring to fix APi probelm

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js - قبلة المسلم PWA Service Worker
-const CACHE_NAME = 'quiblah-muslim-v5';
+const CACHE_NAME = 'quiblah-muslim-v6';
 
 // الأصول الأساسية لتشغيل التطبيق (App Shell)
 const STATIC_ASSETS = [

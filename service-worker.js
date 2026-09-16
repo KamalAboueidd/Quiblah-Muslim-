e4b5274fe9923fc2bc888ce53363cd24e3ac2077
kamal almorsy <kamalaboueidd@gmail.com>
2026-09-17 00:21:09 +0300

    ظبطت طبقة خلفية المسابقات

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js - قبلة المسلم PWA Service Worker
-const CACHE_NAME = 'quiblah-muslim-v37';
+const CACHE_NAME = 'quiblah-muslim-v38';
 
 // الأصول الأساسية لتشغيل التطبيق (App Shell)
 const STATIC_ASSETS = [
